fefc8a3216de3885307497bb9aef5d45436184ab
fwchen <me@chenfangwei.com>
2019-02-15 22:54:44 +0800

    style login page

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public isLogin: Subject<void> = new Subject();
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public login() {
+    
+  }
 }
